a9319b35a25251825000f76cfb81e34106627265
avilavate <avilavate123@gmail.com>
2017-07-14 12:23:31 +0530

    Added custom ruleset to verify dispose implemenetation.

--- with_dispose/Program.cs
+++ with_dispose/Program.cs
@@ -16,21 +16,21 @@ namespace with_dispose
                 loopI.ReadOrderData(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
 
-            //while (true)
-            //{
-            //    var ch = Console.ReadLine();
-            //    if (ch == "x") break;
-            //    else if (ch == "c")
-            //    {
-            //        //DBClass obj = new DBClass();
-            //        //obj.ReadOrderData(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            //        using (DerivedDBClass obj1 = new DerivedDBClass())
-            //        {
-            //            obj1.AllocPointer();
-            //            obj1.ReadOrderData(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            //        }
-            //    }
-            //}
+            while (true)
+            {
+                var ch = Console.ReadLine();
+                if (ch == "x") break;
+                else if (ch == "c")
+                {
+                    //DBClass obj = new DBClass();
+                    //obj.ReadOrderData(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                    using (DerivedDBClass obj1 = new DerivedDBClass())
+                    {
+                        obj1.AllocPointer();
+                        obj1.ReadOrderData(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                    }
+                }
+            }
 
         }
     }
